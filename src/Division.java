9010ccfaa6869b95042f27c91ba173ebc1f17412
Gagan P <gaganprakash.sv@gmail.com>
2024-08-06 18:56:29 +0530

    new update

--- src/Division.java
+++ src/Division.java
@@ -3,10 +3,9 @@ public class Division {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=4;
-		int b=2;
-		System.out.println(a/b);
-		System.out.println("heloo..");
+		int a = 4;
+		int b = 2;
+		System.out.println(a / b);
 
 	}
 
